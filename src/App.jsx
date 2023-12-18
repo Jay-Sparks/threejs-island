ebcaa8e86f3bc9129c2acd0c57af9fae0e975746
Jay-Sparks <jay_sparks@icloud.com>
2023-12-18 18:00:38 +0000

    cleaning up read me and adding dist build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Home, About, Projects, Contact } from './pages';
+import { Home } from './pages';
 import Navbar from './components/Navbar';
 
 const App = () => {
@@ -10,9 +10,6 @@ const App = () => {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
     </main>
